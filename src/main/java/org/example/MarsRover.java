8f8269152c8a67055f13748f2640815f63ef6f63
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-19 15:41:25 +0800

    [MarsRover] feat: execute command given by user

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -17,5 +17,42 @@ public class MarsRover {
         return this.x + ":" + this.y + ":" + this.direction;
     }
 
+    public void executeCommand(String command) {
+        switch (command) {
+            case "L":
+                switch (this.direction) {
+                    case "N":
+                        this.direction = "W";
+                        break;
+                    case "W":
+                        this.direction = "S";
+                        break;
+                    case "S":
+                        this.direction = "E";
+                        break;
+                    case "E":
+                        this.direction = "N";
+                        break;
+                }
+                break;
+            case "R":
+                switch (this.direction) {
+                    case "N":
+                        this.direction = "E";
+                        break;
+                    case "E":
+                        this.direction = "S";
+                        break;
+                    case "S":
+                        this.direction = "W";
+                        break;
+                    case "W":
+                        this.direction = "N";
+                        break;
+                }
+                break;
+        }
+    }
+
 
 }
