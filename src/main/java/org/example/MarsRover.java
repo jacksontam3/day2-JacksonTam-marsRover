f0901ecfbe54a4b3e0cd4e5a3d10ed2e7270fde9
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-19 15:38:29 +0800

    [MarsRover] feat: mars rover class with constructor

---
+++ src/main/java/org/example/MarsRover.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class MarsRover {
+
+    private int x;
+    private int y;
+    private String direction;
+
+
+    public MarsRover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+
+}
