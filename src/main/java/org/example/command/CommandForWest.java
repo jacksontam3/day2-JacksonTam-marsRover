d97427cc47264da9120544565647169a1a9500ff
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-21 00:34:57 +0800

    [MarsRover] feat: adding abstractCommand class with command for each direction

---
+++ src/main/java/org/example/command/CommandForWest.java
@@ -0,0 +1,4 @@
+package org.example.command;
+
+public class CommandForWest extends abstractCommand{
+}
---
+++ src/main/java/org/example/command/abstractCommand.java
@@ -0,0 +1,19 @@
+package org.example.command;
+
+import org.example.constant.Direction;
+
+public  abstract class abstractCommand {
+
+    public static abstractCommand getCommand(Direction direction) {
+        return switch (direction) {
+            case N -> new CommandForNorth();
+            case S -> new CommandForSouth();
+            case E -> new CommandForEast();
+            case W -> new CommandForWest();
+            default -> null;
+        };
+    }
+
+
+
+}
